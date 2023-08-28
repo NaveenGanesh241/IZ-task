c0d46c775fd5b944aba93d5e7d815fad306632f8
NaveenGanesh241 <naveenganesh241@gmail.com>
2023-08-28 16:50:41 -0700

    enum used

---
+++ src/app/component/enum/gender-enum.ts
@@ -0,0 +1,8 @@
+export enum gender{
+    Male='Male',
+    Female='Female',
+    Transgender='Transgender',
+    Non_conforming='Non_conforming',
+    Prefer_not_to_respond='Prefer_not_to_respond'
+  
+}
--- src/app/component/lobby/lobby.component.ts
+++ src/app/component/lobby/lobby.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import {FormGroup,FormControl,Validators,AbstractControl} from '@angular/forms'
 import { Router } from '@angular/router';
+import {gender} from '../enum/gender-enum'
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
   styleUrls: ['./lobby.component.css']
 })
 export class LobbyComponent {
+  Genders: gender[]=[gender.Male,gender.Female,gender.Transgender,gender.Non_conforming,gender.Prefer_not_to_respond]
   constructor(private router:Router){
 
   }
+  
   signinForm={
     mail:'',
     password:''
@@ -28,6 +31,7 @@ export class LobbyComponent {
     email :new FormControl("",[Validators.required,Validators.email,Validators.maxLength(32)]),
     phone: new FormControl("",[Validators.required,Validators.maxLength(10),Validators.minLength(10)]),
     location:new FormControl("",[Validators.required]),
+    gender:new FormControl("",Validators.required),
     password:new FormControl("",[Validators.required,Validators.maxLength(12),Validators.minLength(8)]),
     confirm_password:new FormControl("",[Validators.required,Validators.maxLength(12),Validators.minLength(8)])
   })
